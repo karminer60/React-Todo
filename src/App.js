508ea93c98289166783722e12924766ac96ea700
karina-rodriguez <karminer60@gmail.com>
2020-06-01 19:22:50 -0700

    added template code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      item: ""
+    };
+  }
+
+  handleChanges = e => {
+    // update state with each keystroke
+    this.setState({
+      item: e.target.value
+    });
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
